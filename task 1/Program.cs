ef4015c04976cf28197417cb08e9e79e82b84987
Davron Ziyoev <parsifalking0@gmail.com>
2023-12-19 14:13:22 +0500

    Homework 5

---
+++ task 1/Program.cs
@@ -0,0 +1,38 @@
+﻿Person person = new Person();
+ System.Console.Write("Enter your FirstName : ");
+ person.FirstName = Console.ReadLine();
+ System.Console.Write("Enter your LastName : ");
+ person.LastName = Console.ReadLine();
+ System.Console.Write("Enter your Age : ");
+ person.Age = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("FirstName: " + person.GetInfo1());
+System.Console.WriteLine("LastName: " + person.GetInfo2());
+System.Console.WriteLine("Age: " + person.GetInfo3());
+
+
+public class Person
+{
+public  string FirstName ;
+public string LastName;
+public int Age;
+ 
+ public string GetInfo1()
+ {
+    return $"{FirstName}";
+ }
+ public string GetInfo2()
+ {
+    return $"{LastName} ";
+ }
+ public string GetInfo3()
+ {
+    return $"{Age}";
+ }
+ 
+}
+
+
+
+
+
---
+++ task 2/Program.cs
@@ -0,0 +1,39 @@
+﻿Post post = new Post();
+post.SetPost();
+post.AddLike();
+for (int i = 0; i < post.Comments.Length; i++)
+   {
+    post.Comments[i] = System.Console.ReadLine();
+   }  
+
+System.Console.WriteLine("Text = "  + post.Text );
+System.Console.WriteLine("Likes = " + post.Likes);
+System.Console.WriteLine();
+System.Console.WriteLine("Comments : " );
+System.Console.WriteLine(post.AddComment(post.Comments));
+  public class Post
+{
+public  string Text;
+public int Likes;
+public string[] Comments = new string[2];
+ 
+ public string SetPost()
+ {
+     return Text =  System.Console.ReadLine();
+ }
+ public int AddLike ()
+ {
+   return Likes =  Convert.ToInt32(System.Console.ReadLine());
+ }
+ public string AddComment(string[] Comments)
+ {
+   for (int i = 0; i < Comments.Length; i++)
+   {
+    System.Console.WriteLine(Comments[i]);
+   }
+   return Comments[Comments.Length];
+ }
+
+}
+
+
